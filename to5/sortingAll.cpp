0b259a763822622f35f17a3d12c6d8e812743dc5
ghozimahdi <profghozimahdi@gmail.com>
2020-04-04 10:31:28 +0700

    sort

--- to5/sortingAll.cpp
+++ to5/sortingAll.cpp
@@ -131,6 +131,60 @@ void acakLagi()
     cout << "Data sudah teracak!" << endl;
 }
 
+void merge(int low, int high, int mid)
+{
+    int i, j, k, temp[high - low + 1];
+    i = low;
+    k = 0;
+    j = mid + 1;
+
+    while (i <= mid && j <= high)
+    {
+        if (dataKu[i] < dataKu[j])
+        {
+            temp[k] = dataKu[i];
+            k++;
+            i++;
+        }
+        else
+        {
+            temp[k] = dataKu[j];
+            k++;
+            j++;
+        }
+    }
+
+    while (i <= mid)
+    {
+        temp[k] = dataKu[i];
+        k++;
+        i++;
+    }
+    while (j <= high)
+    {
+        temp[k] = dataKu[j];
+        k++;
+        j++;
+    }
+    for (i = low; i <= high; i++)
+    {
+        dataKu[i] = temp[i - low];
+    }
+}
+
+void mergeSort(int *data, int low, int high)
+{
+    int mid;
+    if (low < high)
+    {
+        mid = (low + high) / 2;
+        mergeSort(data, low, mid);
+        mergeSort(data, mid + 1, high);
+
+        merge(low, high, mid);
+    }
+}
+
 int main()
 {
     int pil;
@@ -148,9 +202,10 @@ int main()
         cout << "4. Selection Sort" << endl;
         cout << "5. Insertion Sort" << endl;
         cout << "6. Quick Sort" << endl;
-        cout << "7. Tampilkan Data" << endl;
-        cout << "8. Acak Data" << endl;
-        cout << "9. Exit" << endl;
+        cout << "7. Merge Sort" << endl;
+        cout << "8. Tampilkan Data" << endl;
+        cout << "9. Acak Data" << endl;
+        cout << "10. Exit" << endl;
         cout << "   Pilihan Anda = ";
         cin >> pil;
 
@@ -158,29 +213,48 @@ int main()
         {
         case 1:
             input();
+            system("pause");
             break;
         case 2:
             bubble_sort();
+            tampil();
+            system("pause");
             break;
         case 3:
             exchange_sort();
+            tampil();
+            system("pause");
             break;
         case 4:
             selection_sort();
+            tampil();
+            system("pause");
             break;
         case 5:
             insertion_sort();
+            tampil();
+            system("pause");
             break;
         case 6:
             quickSort(0, n - 1);
+            tampil();
+            system("pause");
             break;
         case 7:
+            mergeSort(dataKu, 0, n - 1);
             tampil();
+            cout << "merge sort selesai!" << endl;
+            system("pause");
             break;
         case 8:
-            acakLagi();
+            tampil();
+            system("pause");
             break;
         case 9:
+            acakLagi();
+            system("pause");
+            break;
+        case 10:
             exit(0);
             break;
         }
